20d7f3de76cb9d34630afe526471172d18fcddb8
Ldeguzman9 <90979638+Ldeguzman9@users.noreply.github.com>
2022-03-14 13:29:54 -0700

    gallery images

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,9 +1,73 @@
-import React from 'react';
+import React from "react";
+import { Container } from "react-bootstrap";
 
 const Gallery = () => {
-    return(
-        <h1>Gallery</h1>
-    )
-}
+  return (
+    <Container>
+      <img
+        alt="group-lunch"
+        src="https://github.com/nelsonRoberts1968/so_cal_web/blob/main/src/assets/images/Event.jpeg?raw=true"
+        className="gallery mx-1"
+        style={{ width: "48%" }}
+      />
+      <img
+        alt="tanzanian"
+        src="https://github.com/nelsonRoberts1968/so_cal_web/blob/main/src/assets/images/Event10.jpeg?raw=true"
+        className="gallery mx-1"
+        style={{ width: "48%" }}
+      />
+      <img
+        alt="group-meeting"
+        src="https://github.com/nelsonRoberts1968/so_cal_web/blob/main/src/assets/images/Event1.jpeg?raw=true"
+        className="gallery mx-1"
+        style={{ width: "48%" }}
+      />
+      <img
+        alt="women-lunch"
+        src="https://github.com/nelsonRoberts1968/so_cal_web/blob/main/src/assets/images/Event5.jpeg?raw=true"
+        className="gallery mx-1"
+        style={{ width: "48%" }}
+      />
+      <img
+        alt="party"
+        src="https://github.com/nelsonRoberts1968/so_cal_web/blob/main/src/assets/images/Event6.jpeg?raw=true"
+        className="gallery mx-1"
+        style={{ width: "48%" }}
+      />
+      <img
+        alt="group-drinks"
+        src="https://github.com/nelsonRoberts1968/so_cal_web/blob/main/src/assets/images/Event7.jpeg?raw=true"
+        className="gallery mx-1"
+        style={{ width: "48%" }}
+      />
+
+      <img
+        alt="women-dresses"
+        src="https://github.com/nelsonRoberts1968/so_cal_web/blob/main/src/assets/images/Event8.jpeg?raw=true"
+        className="gallery mx-1"
+        style={{ width: "48%" }}
+      />
+      <img
+        alt="tanzanian-women"
+        src="https://github.com/nelsonRoberts1968/so_cal_web/blob/main/src/assets/images/WomenOfGroup2.jpeg?raw=true"
+        className="gallery mx-1"
+        style={{ width: "48%" }}
+      />
+      <img
+        alt="women"
+        src="https://github.com/nelsonRoberts1968/so_cal_web/blob/main/src/assets/images/Event9.jpeg?raw=true"
+        className="gallery mx-1"
+        style={{ width: "48%" }}
+      />
+
+      <img
+        alt="women-dresses"
+        src="https://github.com/nelsonRoberts1968/so_cal_web/blob/main/src/assets/images/WomenofTheGroup.jpeg?raw=true"
+        className="gallery mx-1"
+        style={{ width: "48%" }}
+      />
+    </Container>
+  );
+};
 
 export default Gallery;
