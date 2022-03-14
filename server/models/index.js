d662330e37e55121a88287fb441a3690e494e3d9
khanhlam90 <91096296+khanhlam90@users.noreply.github.com>
2022-03-14 14:34:27 -0700

    temp removal of Post model since it could not load

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,6 @@
 const User = require('./User');
 const Event = require('./Event');
-const Post = require('./Post');
+// const Post = require('./Post');
 const Membership = require('./Membership');
 
-module.exports = { User, Event, Post, Membership };
+module.exports = { User, Event, Membership };
