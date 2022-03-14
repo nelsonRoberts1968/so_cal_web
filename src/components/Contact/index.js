9325b416c62d796c1bf02ff75f56a5fb15fe4a00
Ldeguzman9 <90979638+Ldeguzman9@users.noreply.github.com>
2022-03-14 15:08:01 -0700

    style contact

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,9 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const Contact = () => {
-    return(
-        <h1>Contact</h1>
-    )
-}
+  return <h1 class="form">Contact</h1>;
+};
 
 export default Contact;
