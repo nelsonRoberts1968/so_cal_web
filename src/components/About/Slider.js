c9f9562e15ab094fc144ebe66c9d55e0c523c447
Ldeguzman9 <90979638+Ldeguzman9@users.noreply.github.com>
2022-03-16 19:15:13 -0700

    images slider

--- src/components/About/Slider.js
+++ src/components/About/Slider.js
@@ -4,77 +4,74 @@ import dataSlider from "./dataSlider";
 import Carousel from "react-elastic-carousel";
 
 export default function Slider() {
-   
-        return (
-            <div className="container-slider">
-          <>
-            <Carousel className="image-corousel">
-              <item>
-                <img
-                  alt=""
-                  src="https://github.com/nelsonRoberts1968/so_cal_web/blob/develop/src/assets/images/leadershipimages/img1.jpeg"
-                  width="400"
-                  height="400"
-           
-                />
-                <p>President</p>
-              </item>
-              <item>
-                <img
-                  alt=""
-                  src="https://github.com/nelsonRoberts1968/so_cal_web/blob/develop/src/assets/images/leadershipimages/img2.jpeg"
-                  width="400"
-                  height="400"
-                />
-                <p> Vice President</p>
-              </item>
-              <item>
-                <img
-                  alt=""
-                  src="https://github.com/nelsonRoberts1968/so_cal_web/blob/develop/src/assets/images/leadershipimages/img4.jpeg"
-                />
-                <p>President</p>
-              </item>
-              <item>
-                <img
-                  alt=""
-                  src="https://github.com/nelsonRoberts1968/so_cal_web/blob/develop/src/assets/images/leadershipimages/img8.jpeg"
-                  width="400"
-                  height="400"
-                />
-                <p>President</p>
-              </item>
-              <item>
-                <img
-                  alt=""
-                  src="https://github.com/nelsonRoberts1968/so_cal_web/blob/develop/src/assets/images/leadershipimages/img9.jpeg"
-                  width="400"
-                  height="400"
-                />
-                <p>President</p>
-              </item>
+  return (
+    <div className="container-slider">
+      <>
+        <Carousel className="image-corousel">
+          <item>
+            <img
+              alt="president"
+              src="https://github.com/nelsonRoberts1968/so_cal_web/blob/develop/src/assets/images/leadershipimages/img1.jpeg?raw=true"
+              width="400"
+              height="400"
+            />
+            <p>President</p>
+          </item>
+          <item>
+            <img
+              alt="Vice-president"
+              src="https://github.com/nelsonRoberts1968/so_cal_web/blob/develop/src/assets/images/leadershipimages/img5.jpeg?raw=true"
+              width="400"
+              height="400"
+            />
+            <p> Vice President</p>
+          </item>
+          <item>
+            <img
+              alt=""
+              src="https://github.com/nelsonRoberts1968/so_cal_web/blob/develop/src/assets/images/leadershipimages/img4.jpeg?raw=true"
+            />
+            <p>President</p>
+          </item>
+          <item>
+            <img
+              alt=""
+              src="https://github.com/nelsonRoberts1968/so_cal_web/blob/develop/src/assets/images/leadershipimages/img8.jpeg?raw=true"
+              width="400"
+              height="400"
+            />
+            <p>President</p>
+          </item>
+          <item>
+            <img
+              alt=""
+              src="https://github.com/nelsonRoberts1968/so_cal_web/blob/develop/src/assets/images/leadershipimages/img9.jpeg?raw=true"
+              width="400"
+              height="400"
+            />
+            <p>President</p>
+          </item>
 
-              <item>
-                <img
-                  alt=""
-                  src="https://github.com/nelsonRoberts1968/so_cal_web/blob/develop/src/assets/images/leadershipimages/img5.jpeg"
-                  width="400"
-                  height="400"
-                />
-                <p>President</p>
-              </item>
-              <item>
-                <img
-                  alt=""
-                  src="https://github.com/nelsonRoberts1968/so_cal_web/blob/develop/src/assets/images/leadershipimages/img6.jpeg"
-                  width="400"
-                  height="400"
-      
-                />
-                <p>President</p>
-              </item>
-            </Carousel>
-          </>
+          <item>
+            <img
+              alt="Vice-president"
+              src="https://github.com/nelsonRoberts1968/so_cal_web/blob/develop/src/assets/images/leadershipimages/img12.jpeg?raw=true"
+              width="400"
+              height="400"
+            />
+            <p>President</p>
+          </item>
+          <item>
+            <img
+              alt=""
+              src="https://github.com/nelsonRoberts1968/so_cal_web/blob/develop/src/assets/images/leadershipimages/img6.jpeg?raw=true"
+              width="400"
+              height="400"
+            />
+            <p>President</p>
+          </item>
+        </Carousel>
+      </>
     </div>
   );
 }
