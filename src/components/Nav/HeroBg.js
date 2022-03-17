972c2a9cae7fc42715c4d1fe6aaa11362acfa8f1
Maggie Finnegan <wisner.maggiel@gmail.com>
2022-03-16 20:15:15 -0700

    fixed HeroBg Box bug

--- src/components/Nav/HeroBg.js
+++ src/components/Nav/HeroBg.js
@@ -1,6 +1,4 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
 const bg_timelapse = require('../../assets/images/bg_timelapse.mp4');
 
 function HeroBg() {
