dc4c655d1df72ad71c025486de9df411fbfbe3ab
Maggie Finnegan <wisner.maggiel@gmail.com>
2022-03-20 22:59:48 -0700

    signup form layout done. Open pages>Register.js>troubleshoot

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,13 +1,100 @@
 import React, { useState } from "react";
-import SignUpForm from '../components/SignUpForm'
-
+// import SignUpForm from '../components/SignUpForm'
 import { useMutation } from "@apollo/client";
 import { ADD_USER } from "../utils/mutations";
+import TextField from '../components/TextField';
+import Toolbar from '../components/Toolbar'
+import LayoutHome from '../components/Hero/LayoutHome'
+import Button from '../components/Button';
 
-const SignUp = () => {
+import Auth from "../utils/auth";
+
+const SignUp = (props) => {
+    const [formState, setFormState] = useState({
+        username: "",
+        password: "",
+        email: "",
+    });
+    const [addUser, { error }] = useMutation(ADD_USER);
+
+    // update state based on form input changes
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+
+    // submit form
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const { data } = await addUser({
+                variables: { ...formState }
+            });
+        } catch (e) {
+            console.error(e);
+        }
+
+        //clear form values
+        setFormState({
+            username: '',
+            email: '',
+            password: ''
+        });
+    };
+
     return (
-        <SignUpForm></SignUpForm>
+        <>
+            <LayoutHome>
+                <Toolbar>
+                    <h1>Register</h1>
+                </Toolbar>
+                <form
+                    onSubmit={handleFormSubmit}
+                    style={{ width: '70% ' }}
+
+                >
+                    <TextField
+                        style={{ width: '95%', margin: '5px' }}
+                        type='input'
+                        size='lg'
+                        isRequired='true'
+                        form label='Username'
+                        color='primary'
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        style={{ width: '95%', margin: '5px' }}
+                        type='input'
+                        size='lg'
+                        isRequired='true'
+                        form label='Password'
+                        color='primary'
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        style={{ width: '95%', margin: '5px' }}
+                        type='input'
+                        size='lg'
+                        isRequired='true'
+                        form label='Email'
+                        color='primary'
+                        onChange={handleChange}
+                    />
+                    <Button
+                        size='large'
+                        type="submit"
+                        value="Send"
+                        className="send-button form-control btn btn-primary"
+                    > Log in </Button>
+                </form>
+            </LayoutHome>
+        </>
     )
-}
+};
 
 export default SignUp;
