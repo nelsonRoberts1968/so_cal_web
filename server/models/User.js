90c0aab564ec39d18b50d0ff729b88fa78388406
Nelson Roberts <nellyroberts1968@gmail.com>
2022-03-21 20:51:30 -0700

    commented bcrypt

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
-const bcrypt = require('bcrypt');
+//const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
   firstName: {
@@ -42,7 +42,7 @@ const userSchema = new Schema({
 userSchema.pre('save', async function(next) {
   if (this.isNew || this.isModified('password')) {
     const saltRounds = 10;
-    this.password = await bcrypt.hash(this.password, saltRounds);
+    //this.password = await bcrypt.hash(this.password, saltRounds);
   }
 
   next();
@@ -50,7 +50,7 @@ userSchema.pre('save', async function(next) {
 
 // compare the incoming password with the hashed password
 userSchema.methods.isCorrectPassword = async function(password) {
-  return await bcrypt.compare(password, this.password);
+  //return await bcrypt.compare(password, this.password);
 };
 
 // get events count by user
